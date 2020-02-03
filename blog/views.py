56cd5a5f05efd8c17c00232e61eb5b47486d3361
MarlineKhavele <marlinekhavele6@gmail.com>
2020-02-03 14:50:36 +0300

    Handling dynamic URL Errors

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,16 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import BlogPost
 
 # Create your views here.
 def blog_post_detail_page(request, post_id):
-    obj = BlogPost.objects.get(id=post_id)
+
+    try:
+        obj = BlogPost.objects.get(id=post_id)
+    except BlogPost.DoesNotExists:
+        raise Http404
+    except ValueError:
+        raise Http404
     template_name = "blog_post_detail.html"
     context = {"object": obj}
     return render(request, template_name, context)
