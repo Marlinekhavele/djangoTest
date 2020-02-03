d0e455cb51c6361f68ffd11e24b46bf6acb3085b
MarlineKhavele <marlinekhavele6@gmail.com>
2020-02-03 15:10:13 +0300

    Added a new Database Lookup value

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,9 @@ from django.db import models
 
 class BlogPost(models.Model):
     title = models.TextField()
+    slug = models.SlugField()
     content = models.TextField(null=True, blank=True)
-    
+
 
 
 
