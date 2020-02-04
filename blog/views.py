0cb31643b211afdab332ae1ed05ea3d0a42c2672
MarlineKhavele <marlinekhavele6@gmail.com>
2020-02-04 10:10:48 +0300

    Added Published and draft posts

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404,redirect
+# from django.utils import timezone
 from .forms import BlogPostModelForm
 from .models import BlogPost
 
@@ -18,7 +19,12 @@ from .models import BlogPost
 
 def blog_post_list_view(request):
     # List objects
-    qs = BlogPost.objects.all()
+    # now = timezone.now()
+    qs = BlogPost.objects.all().published() # queryset -> list of python object
+    if request.user.is_authenticated:
+        my_qs = BlogPost.objects.filter(user=request.user)
+        qs = (qs | my_qs).distinct()
+    # qs = BlogPost.objects.filter(publish_date__ite=now)
     template_name = "blog/list.html"
     context = {"object_list": qs}
     return render(request, template_name, context)
