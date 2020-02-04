4b3e398686d40295dbdad92bbb364e63315bc70c
MarlineKhavele <marlinekhavele6@gmail.com>
2020-02-04 10:52:33 +0300

    Image field and uploading images.

--- src/blog/views.py
+++ src/blog/views.py
@@ -33,7 +33,7 @@ def blog_post_list_view(request):
 @staff_member_required
 def blog_post_create_view(request):
     # create objects
-    form = BlogPostModelForm(request.POST or None)
+    form = BlogPostModelForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         obj = form.save(commit=False)
         obj.user = request.user
